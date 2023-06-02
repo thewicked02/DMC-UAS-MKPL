1a680800f84282840e0f0d82a251d9c8fa0ac541
thewicked02 <100459594+thewicked02@users.noreply.github.com>
2023-06-02 21:55:15 +0700

    Continuos-Testing

--- src/main/java/NumberChecker.java
+++ src/main/java/NumberChecker.java
@@ -4,11 +4,19 @@ public class NumberChecker {
     public static final String ODD = "odd";
 
     public static String evenOrOdd(int[] numbers) {
+        int sum = calculateSum(numbers);
+        return isEven(sum) ? EVEN : ODD;
+    }
+
+    private static int calculateSum(int[] numbers) {
         int sum = 0;
         for (int number : numbers) {
             sum += number;
         }
-        String result = sum % 2 == 0 ? EVEN : ODD;
-        return result;
+        return sum;
+    }
+
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
     }
 }
